f0ca05a30c67886f7d9b9d0dd570fd3bf3985722
Adotac <otacool89@gmail.com>
2022-06-22 09:11:48 +0800

    installed pos and lemma, and some test code sample

--- src/app/services/sentimental-analysis.service.ts
+++ src/app/services/sentimental-analysis.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { FirebaseService } from '../services/firebase.service'
+import {lemmatizer} from "lemmatizer";
 
+declare function require(path: string): any;
 @Injectable({
   providedIn: 'root'
 })
@@ -9,10 +11,23 @@ import { FirebaseService } from '../services/firebase.service'
 
 
 export class SentimentalAnalysisService {
+  pos = require('pos');
 
   constructor(private FS: FirebaseService) {
+    // Testing
+    console.log(lemmatizer("unconditionally"));
+    var words = new this.pos.Lexer().lex('This is some sample text. This text can contain multiple sentences.');
+    var tagger = new this.pos.Tagger();
+    var taggedWords = tagger.tag(words);
+    for (let i in taggedWords) {
+      var taggedWord = taggedWords[i];
+      var word = taggedWord[0];
+      var tag = taggedWord[1];
+      console.log(word + " /" + tag);
+    }
+    }
 
-  }
+
 
 
 
